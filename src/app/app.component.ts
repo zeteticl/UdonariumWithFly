f7c66ff2d19ecf98bb5ced7a91deadab05d5a9e7
Zeteticl <zetet@dasd.com>
2021-05-26 03:30:05 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -458,9 +458,10 @@ export class AppComponent implements AfterViewInit, OnDestroy {
 
   toolBox() {
     this.contextMenuService.open(this.pointerDeviceService.pointers[0], [
-      { name: 'カットイン', materialIcon: 'movie_creation', action: () => this.open('CutInSettingComponent') },
-      { name: 'ダイスボット表', materialIcon: 'table_rows', action: () => this.open('DiceRollTableSettingComponent') }
-    ], 'ツールボックス');
+      { name: 'CutIN', materialIcon: 'movie_creation', action: () => this.open('CutInSettingComponent') },
+      { name: '筆記倉庫', materialIcon: 'folder_shared', action: () => this.open('NoteInventoryComponent') },
+      { name: '簡易骰子表', materialIcon: 'table_rows', action: () => this.open('DiceRollTableSettingComponent') }
+    ], '工具箱');
   }
 
   resetPointOfView() {
